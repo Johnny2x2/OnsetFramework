fd9aa55a48b5c4c5f7c44896253c8e27f9022e09
Johnny2x2 <johnlomba1993@gmail.com>
2019-12-31 00:37:45 -0500

    added CreateInteractiveObject

--- ObjectInteraction/servershop.lua
+++ ObjectInteraction/servershop.lua
@@ -88,15 +88,11 @@ AddRemoteEvent("fShopBuy", function(player, shopid, item)
         CallRemoteEvent(player, "MakeNotification", _("shop_success_buy", _(item), price, _("currency")), "linear-gradient(to right, #00b09b, #96c93d)")
         
         if item == "CHAIR_02" then
-        	local object = CreateObject(512, -169529, -38300, 1146)
-			AddObject(object)
-			AddPlayerToObjectRestriction(object, player)
+        	local object = CreateInteractiveObject(player, 512, -169529, -38300, 1146)
         end
         
         if item == "BRIEFCASE_01" then 
-            local object = CreateObject(526 -169529, -38300, 1146)
-            AddObject(object)
-            AddPlayerToObjectRestriction(object, player)
+            local object = CreateInteractiveObject(player, 526 -169529, -38300, 1146)
         end
     end
 end)
