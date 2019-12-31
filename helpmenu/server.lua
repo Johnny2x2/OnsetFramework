bb4c92cc648b12b674971c73c07ce6c743361bf5
Johnny2x2 <johnlomba1993@gmail.com>
2019-12-31 16:43:59 -0500

    FIXED OBJECT INTERACTION
    REQUIRED ADDING TO EN.JSON IN I18N FILE FOR WORD LOG

--- helpmenu/server.lua
+++ helpmenu/server.lua
@@ -4,7 +4,7 @@ end
 AddCommand("help", cmd_commands)
 AddCommand("?", cmd_commands)
 
-AddEvent("OnPlayerJoin", function()
+AddEvent("OnPlayerJoin", function(player)
     AddPlayerChat(player,"Type /help or /? for help menu")
 end)
 
--- openmap/client.lua
+++ openmap/client.lua
@@ -1,7 +1,7 @@
 local Open = false
 local web_ui = CreateWebUI(0, 0, 0, 0, 4, 32);
 
-LoadWebFile(web_ui, "http://asset/openmap/index.html");
+LoadWebFile(web_ui, "http://asset/OnsetFramework/openmap/index.html");
 SetWebAlignment(web_ui, 0.0, 0.0);
 SetWebAnchors(web_ui, 0.0, 0.0, 1.0, 1.0);
 SetWebVisibility(web_ui, WEB_HIDDEN)
@@ -10,12 +10,11 @@ function OnKeyPress(key)
 	if key == "M" then
 		if Open == false then
 			SetWebVisibility(web_ui, WEB_VISIBLE)
-		Open = true
-	else
-		SetWebVisibility(web_ui, WEB_HIDDEN)
-		Open = false
-		end
-			
+			Open = true
+		else
+			SetWebVisibility(web_ui, WEB_HIDDEN)
+			Open = false
+		end		
 	end
 end
 AddEvent("OnKeyPress", OnKeyPress)
